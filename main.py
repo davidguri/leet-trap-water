c013718692f505341bcdc55b67c4ab39c6d68459
David Guri <81036791+davidguri@users.noreply.github.com>
2022-03-21 22:41:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        water = 0
+        
+        def first(rng, condition = lambda x: True):
+            for d in rng:
+                if condition(d):
+                    return d
+        
+        for i in range(len(height)):
+            if height[i] == 0:
+                continue
+                
+            result = first(height[(i + 1):], lambda d: d >= i)
+            
+            
+                    
+            
+            
+            
+            
+            #water += height[i]        
+        return water
